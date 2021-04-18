4fcf3973a80328db9a5683d3a7106da04f4c2e2a
galinos9 <andreaskoub@gmail.com>
2021-04-18 11:52:15 +0300

    New button test

--- app/src/main/java/com/example/virtuallibrary/MainActivity.kt
+++ app/src/main/java/com/example/virtuallibrary/MainActivity.kt
@@ -1,27 +1,39 @@
 package com.example.virtuallibrary
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.virtuallibrary.fragments.*
+import kotlinx.android.synthetic.main.activity_main.*
 
- class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn_wish = findViewById<Button>(R.id.btn_wish)
-        btn_wish.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+        val homeFragment = HomeFragment()
+        val favouritesFragment = FavouritesFragment()
+        val settingsFragment = SettingsFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_favorite -> makeCurrentFragment(favouritesFragment)
+                R.id.ic_settings -> makeCurrentFragment(settingsFragment)
+
+            }
+            true
         }
 
-        val btn_settings = findViewById<Button>(R.id.btn_settings)
-        btn_settings.setOnClickListener {
-            val intent = Intent(this, MainActivity3::class.java)
-            startActivity(intent)
-        }
     }
-}
 
+
+    private fun makeCurrentFragment (fragment: Fragment) =
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper, fragment)
+                commit()
+            }
+}
